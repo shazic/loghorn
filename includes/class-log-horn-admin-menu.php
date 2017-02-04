5f84be4eaf19e35a3d6eef09e0045f5b9bee37bb
Shashank Chattopadhyaya <shashank.chattopadhyaya@gmail.com>
2017-02-05 01:52:59 +0530

    v0.8.30
    Corrected the code errors for the Preview screen.

--- includes/class-log-horn-admin-menu.php
+++ includes/class-log-horn-admin-menu.php
@@ -2308,13 +2308,13 @@ if  ( ! class_exists ( 'Log_Horn_Admin_Menu' )  )  :
 				<div name="loginform" id="loginform">
 					<p>
 						<label for="user_login"><?php _e( 'Username or Email Address' ); ?><br />
-						<input type="text" name="log" id="user_login"<?php echo $aria_describedby_error; ?> class="input" value="<?php echo esc_attr( $user_login ); ?>" size="20" /></label>
+						<input type="text" name="log" id="user_login" class="input" value="<?php //echo esc_attr( $user_login ); ?>" size="20" /></label>
 					</p>
 					<p>
 						<label for="user_pass"><?php _e( 'Password' ); ?><br />
-						<input type="password" name="pwd" id="user_pass"<?php echo $aria_describedby_error; ?> class="input" value="" size="20" /></label>
+						<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
 					</p>
-					<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php checked( $rememberme ); ?> /> <?php esc_html_e( 'Remember Me' ); ?></label></p>
+					<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php //checked( $rememberme ); ?> /> <?php esc_html_e( 'Remember Me' ); ?></label></p>
 					<p class="submit">
 						<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Log In'); ?>" />
 					</p>
